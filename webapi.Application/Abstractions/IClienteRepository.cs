75bec04144599357dbdc3176770bf7a2cb117641
Boris Moreno <boris_marco_moreno@hotmail.com>
2024-03-13 14:08:45 -0500

    cliente existente validacion

--- webapi.Application/Abstractions/IClienteRepository.cs
+++ webapi.Application/Abstractions/IClienteRepository.cs
@@ -7,6 +7,7 @@ public interface IClienteRepository
 {
     Task<List<Cliente>> GetClientes(string userId);
     Task<Cliente> GetCliente(Guid clienteId);
+    Task<Cliente> GetClienteIdentificacion(string numeroIdentificacion);
     GenericResponse AddCliente(Cliente cliente);
     GenericResponse DeleteCliente(Guid clienteId);
 }
